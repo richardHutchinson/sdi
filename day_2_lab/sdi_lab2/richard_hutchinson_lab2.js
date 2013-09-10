c3eb70f395ddd2f90dac3e93eaa398d782633a9a
richard hutchinson <hutchinsonrichard1@gmail.com>
2013-09-10 10:09:14 -0400

    day_2_lab

---
+++ day_2_lab/sdi_lab2/richard_hutchinson_lab2.js
@@ -0,0 +1,24 @@
+//alert("JavaScript works!");
+
+(function(){
+	/*
+		String
+		String with escapes
+		Number
+		Boolean
+	*/
+	
+	var one = 1;
+	var two = 2;
+	
+	var string = "abcd ";
+	var stringEscapes = "efgh \n ijkl ";
+	var varNumber = 1234;
+	var varBooleanTrue = true;
+	var varBooleanFalse = false;
+	
+	console.log(string + stringEscapes + varNumber + varBooleanTrue + varBooleanFalse);
+	
+	console.log(one += two);
+	
+})();
