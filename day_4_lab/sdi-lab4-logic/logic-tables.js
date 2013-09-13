0105527fbfa747ededd9216e97d68b09a27660fd
richard hutchinson <hutchinsonrichard1@gmail.com>
2013-09-13 12:08:44 -0400

    adding day 4 lab

---
+++ day_4_lab/sdi-lab4-logic/logic-tables.js
@@ -0,0 +1,35 @@
+//  NAME:  Enter your name here
+//  DATE:  Enter the current date
+// Scalable Data Infrastructures
+// Day 4 Lab
+//  Assignment 1
+//  Using logical operators
+
+var a = 0;
+var p;
+var q;
+var r;
+
+p = confirm("First value:  Click OK for true or Cancel for false.");
+q = confirm("Second value:  Click OK for true or Cancel for false.");
+r = confirm("Third value:  Click OK for true or Cancel for false.");
+
+/*if (p && q) {
+    console.log("With " + p + " and " + q + ", the outcome is TRUE.");
+} else {
+    console.log("With " + p + " and " + q + ", the outcome is FALSE.");
+};*/
+
+while(a < 1) {
+
+	if (p || (q || r)) {
+	    console.log("With " + p + ", " + q + " and " + r + " the outcome is TRUE.");
+	} else {
+	    console.log("With " + p + ", " + q + " and " + r + " the outcome is FALSE.");
+	};
+	
+	//console.log(p && (q || r));
+	
+	a ++;
+
+}
