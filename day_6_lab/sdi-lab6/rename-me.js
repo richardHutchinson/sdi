e0765a2e4380495457376ffd5d6d0c5ab9713980
richard hutchinson <hutchinsonrichard1@gmail.com>
2013-09-18 12:04:26 -0400

    adding day 6 lab

--- day_6_lab/sdi-lab6/rename-me.js
+++ day_6_lab/sdi-lab6/rename-me.js
@@ -1,2 +1,13 @@
-var globalArray1 = ["A","B","C"];
-var globalArray2 = ["First letter","Second letter","Third letter"];
+var globalArray1 = ["B","A","C"];
+var globalArray2 = ["Second letter","First letter","Third letter"];
+
+function abc(globalArray1,globalArray2) {
+	var sortGlobalArray1 = globalArray1.sort();
+	var sortGlobalArray2 = globalArray2.sort();
+	
+	for(var i = 0; i < globalArray1.length; i++) {
+		console.log(globalArray1[i] + " " + globalArray2[i]);
+	}
+}
+
+abc(globalArray1,globalArray2);
